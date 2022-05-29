ed0cecf4ed4167e21377f6c7a977fec4a4a5a467
Steven <stevendawn98@gmail.com>
2022-05-29 19:43:30 -0400

    did mini lab first run

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+  // 1. add event listener to form
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  // grab form using selector
+  const toDoForm = document.getElementById('create-task-form')
+  
+  // listen for submit event -> submit and then do something
+  toDoForm.addEventListener('submit', handleSubmit)
+  
 });
+
+function handleSubmit(event) {
+  // prevent js from from reloading page after clicking create new task
+  event.preventDefault()
+  console.log('submitted')
+
+  // grab value was typed into textbox (console log to print it to console, not needed) 
+  console.log(event.target['new-task-description'].value)
+
+  // get tasks to show 
+  const taskList = document.getElementById('tasks')
+
+  // create list item element and set element to task list
+  const task = document.createElement('li')
+  // set task text content to value typed in textbox
+  task.textContent = event.target['new-task-description'].value
+
+  // attach to task to task list
+  taskList.append(task)
+
+  // create delete button 
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    task.removeChild();
+  });
+
+  task.append(deleteButton)
+
+  // reset text field back to description
+  event.target['new-task-description'].value = '';
+  // another way to reset text can work for multiple fields
+  // event.target.reset();
+}
